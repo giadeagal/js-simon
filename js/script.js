37a6bd973cd3be917ec8f99442df4bf69c67c164
Gianluca <gl.galassini@gmail.com>
2021-06-14 03:06:46 +0200

    fine progetto

--- js/script.js
+++ js/script.js
@@ -2,8 +2,35 @@
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-docuPrint("printer", "<h1>Simon Game</h1>")
-alert("Tieni bene a mente questi numeri:\n\n" + getRandList(1, 100, 5));
+docuPrint("printer", "<h1>Simon Game</h1>");
+
+// INPUT**********
+var simonList = getRandList(1, 100, 5);
+alert("Tieni bene a mente questi numeri:\n\n" + simonList);
+
+
+// ELABORAZIONE**********
+
+var counter = 30;
+var countdown = setInterval(function(){
+  docuPrint("text", counter);
+  counter--;
+  if (counter <= 0) {
+    clearInterval(countdown);
+    var checked = memoCheck(simonList);
+    var loseText = " Non ti sei ricordato nessuno dei " + simonList.length + " numeri indicati" +  ", le risposte corrette erano: " + simonList;
+    var winText = "Complimenti! Ti sei ricordato tutti e " +  simonList.length + "I numeri indicati! (" + simonList + ")";
+    var halfWinText = "Ti sei ricordato " + checked.length + " numeri su " + simonList.length + ", ecco la lista completa : " + simonList;
+    if (checked.length == 0) {
+        docuPrint("text", loseText);
+    } else if (checked.length == simonList.length) {
+        docuPrint("text", winText);
+    } else {
+        docuPrint("text", halfWinText);
+    }
+  } 
+}, 1000);
+
 
 // FUNZIONI ++++++++++
 function getRandNum(x, y) {
@@ -25,4 +52,16 @@ function getRandList(x, y, a) {
 
 function docuPrint(x, y) {
     document.getElementById(x).innerHTML = y;
+}
+
+function memoCheck(a) {
+    x = [];
+    var askNum;
+    console.log(a)
+    for (var i = 0; i < a.length; i++) {
+        askNum = parseInt(prompt("inserisci uno dei numeri da ricordare"));
+        if (a.includes(askNum)) {
+            x.push(askNum);
+        }
+    } return x;
 }
